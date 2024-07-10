5b6452372e6407f3692aa94e9a63dc5dd8ad42ad
JP Armstrong <jp@armstrong.sh>
2024-07-09 22:21:50 -0400

    reformat

--- rubix.c
+++ rubix.c
@@ -2,57 +2,57 @@
 #include <stdbool.h>
 
 typedef enum sides {
-  TOP, BOTTOM, LEFT, FRONT, RIGHT, BACK
-} Side; 
+    TOP, BOTTOM, LEFT, FRONT, RIGHT, BACK
+} Side;
 
 enum colors {
-  WHITE, YELLOW, GREEN, RED, BLUE, ORANGE
+    WHITE, YELLOW, GREEN, RED, BLUE, ORANGE
 };
 
-const char *color_codes[] = {
-  "\x1B[47m  \x1b[0m\0",
-  "\x1B[43m  \x1b[0m\0",
-  "\x1B[42m  \x1b[0m\0",
-  "\x1B[41m  \x1b[0m\0",
-  "\x1B[44m  \x1b[0m\0",
-  "\x1B[45m  \x1b[0m\0"
+const char *blocks[] = {
+    "\x1B[47m  \x1b[0m\0",
+    "\x1B[43m  \x1b[0m\0",
+    "\x1B[42m  \x1b[0m\0",
+    "\x1B[41m  \x1b[0m\0",
+    "\x1B[44m  \x1b[0m\0",
+    "\x1B[45m  \x1b[0m\0"
 };
 
 typedef struct Cube {
-  int sides[6][3][3];
+    int sides[6][3][3];
 } Cube;
 
 int print_side(Cube cube, Side side, bool padding) {
-    for (int j=0;j<3;j++) {
-      if (padding) printf("      ");
-      for (int k=0;k<3;k++) {
-        int a = cube.sides[side][j][k];
-        printf(color_codes[a]);
-      }
-      printf("\n");
+    for (int j=0; j<3; j++) {
+        if (padding) printf("      ");
+        for (int k=0; k<3; k++) {
+            int a = cube.sides[side][j][k];
+            printf(blocks[a]);
+        }
+        printf("\n");
     }
 }
 
 int print_cube(Cube cube) {
-  print_side(cube, TOP, true);
-  for (int j=0;j<3;j++) {
-    for (Side i=BOTTOM;i<=RIGHT;i++) {
-      for (int k=0;k<3;k++) {
-        int a = cube.sides[i][j][k];
-        printf(color_codes[a]);
-      }
+    print_side(cube, TOP, true);
+    for (int j=0; j<3; j++) {
+        for (Side i=BOTTOM; i<=RIGHT; i++) {
+            for (int k=0; k<3; k++) {
+                int a = cube.sides[i][j][k];
+                printf(blocks[a]);
+            }
+        }
+        printf("\n");
     }
-    printf("\n");
-  }
-  print_side(cube, BACK, true);
+    print_side(cube, BACK, true);
 }
 
 int main() {
-  Cube cube = {};
-  for (int i=TOP;i<=BACK;i++) {
-    for (int j=0;j<3;j++)
-      for (int k=0;k<3;k++)
-        cube.sides[i][j][k] = i;
-  }
-  print_cube(cube);
- }
+    Cube cube = {};
+    for (int i=TOP; i<=BACK; i++) {
+        for (int j=0; j<3; j++)
+            for (int k=0; k<3; k++)
+                cube.sides[i][j][k] = i;
+    }
+    print_cube(cube);
+}
