a3d55c29b14186ef40c2d497941f0408787dae4c
JP Armstrong <jp@armstrong.sh>
2024-07-13 08:37:54 -0400

    updated version

--- rubix.c
+++ rubix.c
@@ -7,29 +7,35 @@
 byte cube[SIZE];
 byte cube_[SIZE]; // prev state
 
+
 // Inspiration: https://stackoverflow.com/questions/500221/how-would-you-represent-a-rubiks-cube-in-code/62558531#62558531
 
+// rotation matrix
 const byte F[] = {6, 3, 0, 7, 4, 1, 8, 5, 2};
 const byte A[][6] = {
-  { -1,   4,   1,   2,   3,  -1},
-  {  2,  -1,   5,  -1,   0,   4},
-  {  3,   0,  -1,   5,  -1,   1},
-  {  4,  -1,   0,  -1,   5,   2},
-  {  1,   5,  -1,   0,  -1,   3},
-  { -1,   2,   3,   4,   1,  -1}
+  { -1,  4,  1,  2,  3, -1},
+  {  2, -1,  5, -1,  0,  4},
+  {  3,  0, -1,  5, -1,  1},
+  {  4, -1,  0, -1,  5,  2},
+  {  1,  5, -1,  0, -1,  3},
+  { -1,  2,  3,  4,  1, -1}
 };
+
+// adjacent edges
 const byte B[][4] = {
-  {0, 1, 2},
-  {0, 3, 6},
-  {2, 5, 8},
-  {6, 7, 8}
+  {0, 1, 2}, // TOP
+  {0, 3, 6}, // LEFT
+  {2, 5, 8}, // RIGHT
+  {6, 7, 8}  // BOTTOM
 };
+
+// perimeter faces
 const byte C[][4] = {
   {0, 0, 0, 0}, // TOP
   {1, 1, 1, 2}, // LEFT
-  {3, 1, 0, 2}, // FRONT
-  {0, 1, 3, 2}, // RIGHT
-  {0, 1, 3, 2}, // BACK
+  {1, 3, 1, 2}, // FRONT
+  {1, 1, 3, 2}, // RIGHT
+  {1, 3, 0, 2}, // BACK
   {3, 3, 3, 3}  // BOTTOM
 };
 
@@ -50,21 +56,19 @@ void rotate(byte f, byte a) {
       byte f_ = A[f][i];
       for(int k=0;k<3;k++) {
          byte s = B[C[f][i_]][k];
-         printf("f: %d, f_: %d, i_: %d, k: %d, s: %d\n", f, f_, i_, k, s);
+         printf("f: %d, f_: %d, i_: %d, k: %d, s: %d, B: %d\n", f, f_, i_, k, s, C[f][i_]);
          *(cube + f_*9 + s) = *(cube_ + i*9 + s);
       }
       i_++;
       printf("-\n");
-      print_cube(cube);
-//     memcpy(cube + A[f][i] * 9, cube_ + i*9, 3);
-//      printf("A: %d, %d, %d\n", f, i, A[f][i]);
-   }
-   print_nums(cube);
-   print_cube(cube);
+    }
   }
 }
 
 int main() {
+
+  printf("RUBIX CUBE 1000 BY JP\nBuild: %s\n", __DATE__);
+
   for (int i=0;i<6;i++) {
     for (int j=0;j<9;j++) {
       cube[i*9+j] = i; // j
@@ -73,16 +77,19 @@ int main() {
 
   char cmd[10] = {0};
   char* cmds = "ulfrbd";
-  while(1) {
-    print_cube(cube);
+  byte skip = 0;
+   while(1) {
+    if (!skip) print_cube(cube);
     printf("> ");
 
     if (fgets(cmd, sizeof(cmd), stdin) == NULL) {
       return 0;
     }
+     
+    if(skip = cmd[0]=='\n')
+      continue;
 
     *cmd = tolower(*cmd);
-  
     if(cmd[0]=='q')
       return 0;
 
